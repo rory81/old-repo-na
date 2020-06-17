fdfc44e15854cc6b3fd33d8136c65b7ef288e55b
Nathalie <nbleijie@gmail.com>
2020-06-17 22:22:13 +0000

    start with header

--- growing_funds/urls.py
+++ growing_funds/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from projects import views
 
 urlpatterns = [
@@ -25,4 +27,4 @@ urlpatterns = [
     # path('edit/<project_id>', views.edit_project, name='edit'),
     # path('delete/<project_id>', views.delete_project, name='delete'),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
